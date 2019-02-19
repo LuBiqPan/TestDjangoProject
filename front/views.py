06ac85c3f85e5c2e11ac608d8a85d50f1f57dfa3
LuBiqPan <xuyuanyefeng@163.com>
2019-02-19 10:57:56 +0800

    Replace "Lu.Biq La" with "Lu.Biq Pan"

--- front/views.py
+++ front/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        "username": "Lu.Biq La"
+        "username": "Lu.Biq Pan"
     }
     return render(request, 'index.html', context)
