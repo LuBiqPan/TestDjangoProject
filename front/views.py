69e54c4f299b078b23df76561111ef612907039c
LuBiqPan <xuyuanyefeng@163.com>
2019-02-19 10:27:27 +0800

    1

--- front/views.py
+++ front/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        "username": "Lu.Biq Pan"
+    }
+    return render(request, 'index.html', context)
